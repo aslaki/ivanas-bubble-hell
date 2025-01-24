eeb6a7dbfe51326a58b849340119074bba629f57
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-01-24 23:44:32 +0200

    feat: randomize spawn points function

--- Assets/Scripts/Spawner/BubbleSpawner.cs
+++ Assets/Scripts/Spawner/BubbleSpawner.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using System;
 
 public class BubbleSpawner : MonoBehaviour
 {
@@ -12,8 +14,35 @@ public class BubbleSpawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnBubble();
+        SpawningStart();
     }
+
+    public void SpawningStart()
+    {
+        RandomizeSpawnpoints(spawnPoints);
+    }
+
+    private void RandomizeSpawnpoints<T>(IList<T> spawnPoints)
+    {
+        int pointCount = spawnPoints.Count;
+
+        System.Random random = new System.Random();
+
+        for(int i = pointCount - 1; i > 1; i--)
+        {
+            int rnd = random.Next(i + 1);
+
+            T value = spawnPoints[rnd];
+            spawnPoints[rnd] = spawnPoints[i];
+            spawnPoints[i] = value;
+        }       
+    }
+
+ 
+
+
+
+    
 
     private void SpawnBubble()
     {
