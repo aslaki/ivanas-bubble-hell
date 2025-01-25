0787ea3b3820460508826e3f7df468c700ee0d01
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-01-25 20:50:58 +0200

    sounds imported

---
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+
+    [SerializeField] private AudioClip[] lockAudioClips;
+}
