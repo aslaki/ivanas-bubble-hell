b288c0d13c119871595265b71ea8b356a44ca1eb
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-01-25 20:57:08 +0200

    LockSoundEffects function

--- Assets/Scripts/Player/PlayerAudio.cs
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -5,4 +5,29 @@ public class PlayerAudio : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
 
     [SerializeField] private AudioClip[] lockAudioClips;
+
+    private int lockSoundState;
+
+    public void LockSoundEffects()
+    {
+        if(lockSoundState == 0)
+        {
+            audioSource.clip = lockAudioClips[lockSoundState];
+            audioSource.Play();
+            lockSoundState++;
+        }
+        else if(lockSoundState == 1)
+        {
+            audioSource.clip = lockAudioClips[lockSoundState];
+            audioSource.Play();
+            lockSoundState++;
+        }
+
+        else if(lockSoundState == 2)
+        {
+            audioSource.clip = lockAudioClips[lockSoundState];
+            audioSource.Play();
+            lockSoundState++;
+        }
+    }
 }
