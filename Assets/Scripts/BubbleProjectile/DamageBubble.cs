81542590610a124660214ff70d11b1d9f4e83bcb
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-02-09 21:11:56 +0200

    cauldron splash added

--- Assets/Scripts/BubbleProjectile/DamageBubble.cs
+++ Assets/Scripts/BubbleProjectile/DamageBubble.cs
@@ -5,6 +5,7 @@ public class DamageBubble : MonoBehaviour
 
     [SerializeField]
     int damage;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Scripts/Player/CageDeathTrigger.cs
+++ Assets/Scripts/Player/CageDeathTrigger.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class CageDeathTrigger : MonoBehaviour
 {
     [SerializeField] private PlayerAudio playerAudio;
+    [SerializeField] private GameObject cauldronSplash;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("CageKiller"))
         {
             playerAudio.CageDeathSound();
+            Instantiate(cauldronSplash, transform.position, cauldronSplash.transform.rotation);
         }
     }
 }
